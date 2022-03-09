8a603300223de9a22918880aeaa496b904c7470c
Samuel Proulx <samuelproulx26@gmail.com>
2022-03-08 23:58:15 -0500

    helper for debugging and tuning

--- ball_tracker/detected_contour.py
+++ ball_tracker/detected_contour.py
@@ -1,9 +1,11 @@
+import numpy as np
+
 from ball_tracker.enum.colors_enum import Colors
 
 
 class DetectedContour:
     """Structure to represent a contour detected in the pipeline"""
-    def __init__(self, contour, x, y, radius, color: Colors.value):
+    def __init__(self, contour, x, y, radius, color: Colors):
         self.contour = contour
         self.x = x
         self.y = y
